9a12cae56874430214e3cb147cbae0171fb698e7
Mirela Iclodean <miclodean@twitter.com>
2018-08-25 16:02:19 +0100

    Added testcase

--- SherlockAndAnagrams.scala
+++ SherlockAndAnagrams.scala
@@ -4,11 +4,12 @@ rearranged to form the other string. Given a string, find the number of pairs
 of substrings of the string that are anagrams of each other.
 
 For example s = mom, the list of all anagrammatic pairs is [m,m], [[m,o],[o,m]] at positions [0,2] and [[0,1], [1,2]]  respectively.
-input: abcd
+Test case 1:
+Input: abcd
 Arrays: [a], [b] [c] [d]
 Arrays: [ab], [bc] [cd]
 Arrays: [abc], [bcd]
-
+Result -> 0
 */
 
 import scala.collection.mutable.{ ArrayBuffer, Map }
