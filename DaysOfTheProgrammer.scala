4828732b51429fc50c86c385e3d62e08538eccac
Mirela Iclodean <miclodean@twitter.com>
2018-09-21 21:57:08 +0100

    Implement case of 1918 in DaysOfTheProgrammer

--- DaysOfTheProgrammer.scala
+++ DaysOfTheProgrammer.scala
@@ -1,7 +1,7 @@
 
 
 object DaysOfTheProgramer {
-    final val months = List(
+    val months = List(
         31,
         28,
         31,
@@ -16,24 +16,31 @@ object DaysOfTheProgramer {
         31
     )
 
-    final val programerDay: Int = 256
+    val programerDay: Int = 256
 
     def dayOfProgrammer(year: Int): String = {
         val start = if (isLeapYear(year)) 1 else 0
         val month = "%02d".format((programerDay.toDouble / 30).ceil.toInt)
+
         val sum = months.foldLeft(start)( (a, b) => { if ( a + b < programerDay) a + b else a})
-        val day = "%02d".format((programerDay - sum).toInt)
+
+        // Extra days if the year is 1918: Feb 14th was the 32nd day of the year (1st of Feb)
+        val daysToAdd: Int = if (year == 1918) 13 else 0 // 14th of feb to become 1st of feb
+        val day = "%02d".format((programerDay - sum + daysToAdd).toInt)
+
+
         Seq(day, month, year).mkString(".")
     }
 
     def isLeapYear(year: Int): Boolean = {
         year match {
-            case y: Int if (year < 1918 &&  year > 1700) =>
+            case y: Int if (year > 1917) =>
+                y % 4 == 0
+            case _ =>
                 // Divisible by 400
-                (y % 400 == 0 ) ||
-                // Divisible by 
-                (y % 4 == 0 && y % 100 != 0)
-            case _ => year % 4 == 0 
+                (year % 400 == 0 ) ||
+                // Divisible by
+                (year % 4 == 0 && year % 100 != 0)
         }
     }
 
