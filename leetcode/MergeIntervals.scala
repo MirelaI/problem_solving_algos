a75ae7a251e983e14c93fb3c0aeee636e747fb54
Mirela Iclodean <miclodean@twitter.com>
2018-11-05 17:54:42 +0000

    [LeetCode] Merge intervals - stack implementation

--- leetcode/MergeIntervals.scala
+++ leetcode/MergeIntervals.scala
@@ -11,8 +11,42 @@
 // Output: [[1,5]]
 // Explanation: Intervals [1,4] and [4,5] are considerred overlapping.
 
+import scala.collection.mutable.Stack
+import Math.{max, min}
+
 object Solution {
-  def mergeIntervals(left: Array[String], right: Array[String]): Array[String] = {
-    // add implementation
+  def mergeIntervals(intervals: List[List[Int]]): List[List[Int]] = {
+    // Implement a stack
+    val mergedIntervals = Stack[List[Int]]()
+
+    for (interval <- intervals) {
+      // First entry case
+      if (mergedIntervals.headOption.isEmpty) {
+        mergedIntervals.push(interval)
+      } else {
+        // [[1,3],[2,6],[8,10],[15,18]]
+        if (mergedIntervals.head(1) >= interval(1) && mergedIntervals.head(0) <= interval(0)) {
+          // go to the next entry
+        } else if (mergedIntervals.head(1) >= interval(0) && mergedIntervals.head(1) <= interval(1)) {
+          val headOfStack = mergedIntervals.pop
+          val startOfMerged = min(headOfStack(0), interval(0))
+          val endOfMerged = max(headOfStack(1), interval(1))
+
+          val mergedInt = List(startOfMerged, endOfMerged)
+          mergedIntervals.push(mergedInt)
+        } else {
+          mergedIntervals.push(interval)
+        }
+      }
+    }
+
+    mergedIntervals.toList.reverse
+  }
+
+  def main(args: Array[String]): Unit = {
+    val intervals = List(List(1,3), List(2,6), List(8, 10), List(15, 18))
+    val mergedIntervals: List[List[Int]] = mergeIntervals(intervals)
+
+    println(mergedIntervals)
   }
 }
--- leetcode/NumberOfIslands.scala
+++ leetcode/NumberOfIslands.scala
@@ -21,6 +21,15 @@
 
 // Output: 3
 
+// Approach #1 DFS [Accepted]
+// Intuition
+
+// Treat the 2d grid map as an undirected graph and there is an edge between two horizontally or vertically adjacent nodes of value '1'.
+
+// Algorithm
+
+// Linear scan the 2d grid map, if a node contains a '1', then it is a root node that triggers a Depth First Search. During DFS, every visited node should be set as '0' to mark as visited node. Count the number of root nodes that trigger DFS, this number would be the number of islands since each DFS starting at some root identifies an island.
+
 object Solution {
     def numIslands(grid: Array[Array[Char]]): Int = {
 
